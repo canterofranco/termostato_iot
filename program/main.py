343a4aef0270b807999a5f6f689797f346f0764a
Cantero Franco <canterofrancoj@gmail.com>
2026-05-05 16:34:52 -0300

    correcciones en la funcion main

--- program/main.py
+++ program/main.py
@@ -20,7 +20,6 @@ rele = machine.Pin(16, machine.Pin.OUT, value=1)
 sensor = dht.DHT22(machine.Pin(15))
 
 
-
 estado = { #parametros no volatiles
     'setpoint': 20,
     'periodo':20, #cada tiempo publica 
@@ -32,15 +31,8 @@ cargar_estado(estado)
 
 async def main(client):
     await client.connect()
-    n = 0
     await asyncio.sleep(2)  # Give broker time
-    while True:
-        print('publish', n)
-        # If WiFi is down the following will pause for the duration.
-        await client.publish(ID_DISPOSITIVO, '{} {}'.format(n, client.REPUB_COUNT), qos = 1)
-        n += 1
-        await asyncio.sleep(10)  # Broker is slow
-
+    
     await client.subscribe(ID_DISPOSITIVO + "/setpoint", qos=1)
     await client.subscribe(ID_DISPOSITIVO + "/periodo", qos=1)
     await client.subscribe(ID_DISPOSITIVO + "/destello", qos=1)
@@ -51,9 +43,8 @@ async def main(client):
 
     asyncio.create_task(medir(sensor, estado))
     asyncio.create_task(publicar(client, estado, ID_DISPOSITIVO))
-
     asyncio.create_task(control_termostato(rele, estado))
-
+    
     asyncio.create_task(
         escuchar_mensajes(
             client,
